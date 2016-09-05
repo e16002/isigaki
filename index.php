9ade80d4036817f9bbfee46eb79f3219d8920b5d
kayou yuuki <e16002@std.it-college.ac.jp>
2016-09-05 20:48:16 +0900

    isigaki databass sakuse

--- index.php
+++ index.php
@@ -84,7 +84,7 @@
                         </p>
                         <p>[<a href="single.html">続きを読む</a>]</p>
                     </div>
-                    <figure><a href="single.html"><img src="images/dummy/180x180-2.png" height="180" width="180" alt=""></a></figure>
+                    <figure><a href="single.html"><img src="<?php echo get_template_directory_uri(); ?>/images/dummy/180x180-2.png" height="180" width="180" alt=""></a></figure>
                 </article><!-- /.news -->
 
                 <article class="news">
