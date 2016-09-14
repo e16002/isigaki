699602b464005d3fe41d5a469b614e618a41889e
kayou yuuki <e16002@std.it-college.ac.jp>
2016-09-14 19:30:29 +0900

    サイドバーのリンク設定

--- sidebar.php
+++ sidebar.php
@@ -1,6 +1,6 @@
 <aside class="subContents">
             <p class="bnr">
-                <a href="contact.html"><img src="<?php echo get_template_directory_uri(); ?>/images/common/bnr_contact01.png" height="183" width="300" alt="宿泊のお問い合わせ"></a>
+                <a href="<?php echo get_permalink(27); ?>"><img src="<?php echo get_template_directory_uri(); ?>/images/common/bnr_contact01.png" height="183" width="300" alt="宿泊のお問い合わせ"></a>
             </p>
             <div class="wrapper">
                 <section class="socialBanners">
